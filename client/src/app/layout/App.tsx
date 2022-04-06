54078a59417689b768288bdeb9adee9c900968d5
jcpayano31 <jcpayano31@gmail.com>
2022-04-06 15:48:00 -0400

    End Section 4

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,6 +1,12 @@
+
 import { Container, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import { useState } from "react";
+import { Route, Routes  } from "react-router-dom";
+import AboutPage from "../../features/about/AboutPage";
 import Catalog from "../../features/catalog/Catalog";
+import ProductDetails from "../../features/catalog/ProductDetails";
+import ContactPage from "../../features/contact/ContactPage";
+import HomePage from "../../features/home/HomePages";
 import Header from "./Header";
 
 
@@ -25,7 +31,13 @@ function handleThemeChange() {
       <CssBaseline/>
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
       <Container>
-        <Catalog />
+        <Routes>
+          <Route path='/' element={<HomePage/>} />
+          <Route path='/catalog' element={<Catalog/>} />
+          <Route path='/catalog/:id' element={<ProductDetails/>} />
+          <Route path='/about' element={<AboutPage/>} />
+          <Route path='/contact' element={<ContactPage/>} />
+        </Routes>      
       </Container>    
      
     </ThemeProvider>
